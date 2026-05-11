331c81878576eb5d29a77fbd194f28ea357b406d
Muzammil Shah <120221116+Techno69104@users.noreply.github.com>
2026-05-11 21:51:24 +0500

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# render-build.sh
-
-# Install system dependencies for PyMuPDF
-apt-get update
-apt-get install -y python3-dev libjpeg-dev zlib1g-dev
-
-# Install Python packages
-pip install -r backend/requirements.txt
